62719dd2c0d8f3e7a87d296380f38ccbf95273cc
NSokolov114 <sokoloff114@gmail.com>
2022-02-23 21:17:35 +0300

    feat user cards search functionality for user cards

--- rnd-users-cards/src/App.js
+++ rnd-users-cards/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -8,7 +9,10 @@ class App extends Component {
 
     this.state = {
       users: [],
+      searchField: '',
     };
+
+    // this.handleChange = this.handleChange.bind(this); // in constructor
   }
 
   componentDidMount() {
@@ -17,15 +21,23 @@ class App extends Component {
       .then(users => this.setState({ users: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { users, searchField } = this.state;
+    const filteredUsers = users.filter(user => {
+      return user.name.toLowerCase().includes(searchField.toLowerCase());
+    });
     return (
       <div className="App">
-        <CardList name="vasya">
-          <h1>Hello</h1>
-        </CardList>
-        {this.state.users.map(user => (
-          <h1 key={user.id}>{user.name}</h1>
-        ))}
+        <h1>Best Workers on the Month</h1>
+        <SearchBox
+          placeholder="search users"
+          handleChange={this.handleChange}
+        />
+        <CardList users={filteredUsers} />
       </div>
     );
   }
--- rnd-users-cards/src/components/card-list/card-list.component.jsx
+++ rnd-users-cards/src/components/card-list/card-list.component.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import './card-list.styles.css';
+import { Card } from '../card/card.component';
 
 export const CardList = props => {
   console.log(props);
-  return <div>{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.users.map(user => (
+        <Card key={user.id} user={user} />
+      ))}
+    </div>
+  );
 };
