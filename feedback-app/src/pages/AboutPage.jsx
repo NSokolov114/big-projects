6a3df29e98de8bf8fe0a36336e3a5a6bc845424a
NSokolov114 <sokoloff114@gmail.com>
2022-02-26 21:03:10 +0300

    feat routing for the feedback app

---
+++ feedback-app/src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import Card from '../shared/Card';
+import { Link } from 'react-router-dom';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React app to leave a feedback for a service</p>
+        <p>Version: 1.1.1</p>
+        <p>
+          <Link to="/">Back To Home</Link>
+        </p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
