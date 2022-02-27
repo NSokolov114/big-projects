0da97a8d20dc6d09c53258a1652f90caa74614e5
NSokolov114 <sokoloff114@gmail.com>
2022-02-27 22:11:27 +0300

    feat routing for the github finder app

---
+++ github-finder/src/pages/About.jsx
@@ -0,0 +1,30 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">A React app to search GitHub profiles and see profile details.</p>
+      <p className="mb-4 text-2xl font-light">
+        This project is part of the
+        <strong>
+          <a href="https://www.udemy.com/course/modern-react-front-to-back/"> React course on Udemy</a>
+        </strong>{' '}
+        by
+        <strong>
+          <a href="https://traversymedia.com"> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className="text-lg text-gray-400">
+        Version:&nbsp;<span className="text-white">1.1</span>
+      </p>
+      <p className="text-lg text-gray-400">
+        Layout By:&nbsp;
+        <a className="text-white" href="https://twitter.com/hassibmoddasser">
+          Hassib Moddasser
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default About;
